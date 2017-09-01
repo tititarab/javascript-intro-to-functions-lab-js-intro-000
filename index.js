8c87c3da508f66303bb3d580af696fd440822d03
Tiffany Tarab <tiffany.tarab@gmail.com>
2017-09-01 14:24:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function whisper(string) {
 
 function sayHiToGrandma(string) {
   if ("string".toLowerCase())
-  return "I can't hear you!"
+  return "I can\'t hear you!"
   else if ("string".toUpperCase())
   return "YES INDEED"
   else if ("I love you, Grandma.")
