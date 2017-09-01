232edba2acfc58e96597a90ec6fa39f4494f0a6f
Tiffany Tarab <tiffany.tarab@gmail.com>
2017-09-01 14:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function sayHiToGrandma(string) {
   return "I can't hear you!"
   else if ("string".toUpperCase())
   return "YES INDEED"
-  else if "I love you, Grandma."
+  else if ("I love you, Grandma.")
   return "I love you, too."
 }
